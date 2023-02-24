560521885ff93e45033f0dbaaf70a06fd0316d65
jopejoe1 <johannes@joens.email>
2023-02-24 18:33:14 +0100

    add git

--- flake.nix
+++ flake.nix
@@ -138,7 +138,7 @@
            # useGlobalPkgs = true;
            # useUserPackages = true;
           #};
-          system.stateVersion = "22.11";
+          system.stateVersion = "23.05";
         }
 
       ];
--- systems/x86_64-linux/kami/default.nix
+++ systems/x86_64-linux/kami/default.nix
@@ -139,6 +139,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    git
   ];
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
