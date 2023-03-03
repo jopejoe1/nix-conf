c064913bb3e814abdd7f8c487a2ce858d7fe89fa
jopejoe1 <johannes@joens.email>
2023-03-03 12:07:03 +0100

    Use hash instead of sha

--- packages/modrinth/knossos/default.nix
+++ packages/modrinth/knossos/default.nix
@@ -8,7 +8,7 @@ buildNpmPackage {
     owner = "modrinth";
     repo = "knossos";
     rev = "v2.2";
-    sha256 = "sha256-p6rHMio9oOmT2qyPq+TPzd7id+X1fck9Wa3vLHWp0Kg=";
+    hash = "sha256-p6rHMio9oOmT2qyPq+TPzd7id+X1fck9Wa3vLHWp0Kg=";
   };
 
   npmDepsHash = "sha256-4ha6qgehExf/OoUGu622vpBTN80/83r3IWbPbBOSCMQ=";
