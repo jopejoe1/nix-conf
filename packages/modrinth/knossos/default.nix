775f1f099a0dfb63cb6e656dd65fa248f72b3bcd
jopejoe1 <johannes@joens.email>
2023-03-03 12:02:52 +0100

    Maybe this works

--- packages/modrinth/knossos/default.nix
+++ packages/modrinth/knossos/default.nix
@@ -1,13 +1,12 @@
 { lib, fetchFromGitHub, buildNpmPackage, ... }:
 
-buildNpmPackage
-{
+buildNpmPackage {
   name = "knossos";
   verison = "2.2";
 
   src = fetchFromGitHub {
     owner = "modrinth";
-    repo = name;
+    repo = "${name}";
     rev = "v${version}";
     sha256 = "sha256-nJDte8rpYq3Ge844qtAOvLp6NcFsl51jFgaZGR97/YI";
   };
