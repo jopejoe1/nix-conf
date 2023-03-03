2fd71ec9d1c4a0543914823ace10227589c07776
jopejoe1 <johannes@joens.email>
2023-03-03 12:06:40 +0100

    Update Hash

--- packages/modrinth/knossos/default.nix
+++ packages/modrinth/knossos/default.nix
@@ -11,5 +11,5 @@ buildNpmPackage {
     sha256 = "sha256-p6rHMio9oOmT2qyPq+TPzd7id+X1fck9Wa3vLHWp0Kg=";
   };
 
-  #npmDepsHash = "sha256-RdRQMrYoOaf2rjhvVpZw0skcekKL8rzG3oFlf/1D1cY";
+  npmDepsHash = "sha256-4ha6qgehExf/OoUGu622vpBTN80/83r3IWbPbBOSCMQ=";
 }
