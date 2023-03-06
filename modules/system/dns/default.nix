88eb38fba361e4b2d26dc5941bf150056a09dee3
jopejoe1 <johannes@joens.email>
2023-03-06 13:52:34 +0100

    Forgot to commit dns module

---
+++ modules/system/dns/default.nix
@@ -0,0 +1,24 @@
+{ options, config, pkgs, lib, ... }:
+
+with lib;
+#with lib.internal;
+let cfg = config.custom.system.dns;
+in
+{
+  options.custom.system.dns = with types; {
+    enable = mkBoolOpt false "Whether or not to enable creation of dns server.";
+  };
+
+  config = mkIf cfg.enable {
+    services.bind = {
+      enable = true;
+      services.bind.forwarders = [];
+      zones = {
+        "." = {
+          file = pkgs.dns-root-data.path + "/root.hints";
+        };
+      };
+    };
+  };
+}
+
