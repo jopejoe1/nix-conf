2821877923678872860df2819734e48cd809e604
jopejoe1 <johannes@joens.email>
2023-03-14 13:51:23 +0100

    Add iso installer config

---
+++ systems/aarch64-install-iso/graphical-arm/default.nix
@@ -0,0 +1,36 @@
+{ pkgs, config, lib, channel, ... }:
+
+with lib;
+#with lib.internal;
+{
+  networking.networkmanager.enable = true;
+
+  hardware.opengl.enable = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+
+  environment.systemPackages = with pkgs; [
+    gparted
+    parted
+    gnufdisk
+    partition-manager
+  ]
+
+  hardware.enableRedistributableFirmware = true;
+
+  programs = {
+    dconf.enable = true;
+    xwayland.enable = true;
+  };
+
+  # System
+  custom.system.boot.uboot.enable = true;
+  custom.system.ssh.enable = true;
+  custom.nix.enable = true;
+  custom.hardware.audio.enable = true;
+  custom.desktop.plasma.enable = true;
+
+  # User
+  custom.user.jopejoe1.enable = true;
+  custom.user.root.enable = true;
+}
