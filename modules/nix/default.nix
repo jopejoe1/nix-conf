c5674b3463d2f991557d7932cdd24609807b49df
jopejoe1 <johannes@joens.email>
2023-04-24 17:55:07 +0200

    Add nixos search cache

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -25,14 +25,16 @@ in
       package = cfg.package;
       settings = {
         substituters = [
-          "https://cache.nixos.org/"
+          "https://cache.nixos.org"
           "https://nix-community.cachix.org"
           "https://prismlauncher.cachix.org"
+          "https://nixos-search.cachix.org"
         ];
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "prismlauncher.cachix.org-1:GhJfjdP1RFKtFSH3gXTIQCvZwsb2cioisOf91y/bK0w="
+          "nixos-search.cachix.org-1:1HV3YF8az4fywnH+pAd+CXFEdpTXtv9WpoivPi+H70o="
         ];
         trusted-users = [ "root" ];
         sandbox = true;
