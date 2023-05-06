5cd2cb2072ba3d894bbada22d3545e58c6bfa461
jopejoe1 <johannes@joens.email>
2023-05-06 15:02:37 +0200

    Use Prism Ovveride

--- flake.nix
+++ flake.nix
@@ -135,7 +135,7 @@
       overlays = with inputs; [
         nur.overlay
         snowfall-flake.overlay
-        #prismlauncher.overlay # Temporly as its broken currently
+        prismlauncher.overlay # Temporly as its broken currently
       ];
 
       systems.modules = with inputs; [
