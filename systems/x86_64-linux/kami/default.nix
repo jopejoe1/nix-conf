d32613fffe5f99a1b7ebb66c9d75a323aad1c485
jopejoe1 <johannes@joens.email>
2023-05-11 11:58:29 +0200

    Remove prismlauncher ovveride

--- systems/x86_64-linux/kami/default.nix
+++ systems/x86_64-linux/kami/default.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, prismlauncher, ... }:
+{ config, pkgs, ... }:
 
 {
   imports =
@@ -35,7 +35,7 @@
   services.boinc.enable = true;
 
   environment.systemPackages = with pkgs; [
-    prismlauncher.packages.${pkgs.system}.prismlauncher
+    prismlauncher
     libsForQt5.discover
     skypeforlinux
     discord
