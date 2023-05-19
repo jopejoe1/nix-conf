bd79f8f36b02b53830dadef744b3d8b7bcef3639
jopejoe1 <johannes@joens.email>
2023-05-19 18:29:28 +0200

    Clean Up

--- flake.nix
+++ flake.nix
@@ -1,26 +1,6 @@
 {
   description = "jopejoe1 NixOS configuration";
 
-  nixConfig = {
-    experimental-features = [ "nix-command" "flakes" ];
-    substituters = [
-      "https://cache.nixos.org"
-    ];
-
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-      "https://prismlauncher.cachix.org"
-      "https://nixos-search.cachix.org"
-    ];
-
-    extra-trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "prismlauncher.cachix.org-1:GhJfjdP1RFKtFSH3gXTIQCvZwsb2cioisOf91y/bK0w="
-      "nixos-search.cachix.org-1:1HV3YF8az4fywnH+pAd+CXFEdpTXtv9WpoivPi+H70o="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
     nur.url = github:nix-community/NUR;
