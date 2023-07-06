066f9152c8dde41af999bde2a028ddb6332e3c9d
jopejoe1 <johannes@joens.email>
2023-07-06 11:03:01 +0200

    Remove docker

--- systems/kami/default.nix
+++ systems/kami/default.nix
@@ -88,10 +88,4 @@
 
   # https://github.com/NixOS/nixpkgs/blob/c32c39d6f3b1fe6514598fa40ad2cf9ce22c3fb7/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix#L66
   boot.loader.systemd-boot.editor = false;
-
-  virtualisation.docker = {
-    enable = true;
-    enableNvidia = true;
-    package = pkgs.podman;
-  };
 }
