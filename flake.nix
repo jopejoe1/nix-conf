70bb43250bf16b298308ae83dcbf0240f37ed16f
jopejoe1 <johannes@joens.email>
2023-07-12 13:50:49 +0200

    Add discord override

--- flake.nix
+++ flake.nix
@@ -85,7 +85,11 @@
                   });
                   discord = super.discord.overrideAttrs (old: {
                     desktopItem = old.desktopItem.override (old: { exec = old.exec + " --disable-gpu-sandbox"; });
-                  });
+                  }).override {
+                    withOpenASAR = true;
+                    withVencord = true;
+                    withTTS = true;
+                  };
                 })
               ];
             };
