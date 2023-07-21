ca67be035d628868e417cdf907d43cac5ed59e3a
jopejoe1 <johannes@joens.email>
2023-07-22 00:46:31 +0200

    Change modt

--- systems/kami/default.nix
+++ systems/kami/default.nix
@@ -38,7 +38,7 @@
       difficulty = 3;
       enable-rcon = true;
       "rcon.password" = "test";
-      motd = "Wake up to reality! Nothing ever goes as planned in this accursed world.";
+      motd = "&cWake up to reality! Nothing ever goes as planned in this accursed world.☯️";
       spawn-protection = 0;
       level-type = "minecraft:amplified";
       level-name = "amplified_world";
