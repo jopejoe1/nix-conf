b084655db4a92263b3c213c7f74aa883fc912790
jopejoe1 <johannes@joens.email>
2023-08-14 17:39:06 +0200

    forgot to add .git add the end

--- modules/auto-update/default.nix
+++ modules/auto-update/default.nix
@@ -4,9 +4,7 @@
   system.autoUpgrade = {
     enable = true;
     randomizedDelaySec = "30min";
-    flake = "git+https://codeberg.org/jopejoe1/nix-conf";
+    flake = "git+https://codeberg.org/jopejoe1/nix-conf.git";
     dates = "hourly";
   };
 }
-
-
