a04a233427613bb52a8884b385bb5f9d185b8fa0
jopejoe1 <johannes@joens.email>
2023-08-14 17:35:35 +0200

    clean up

--- systems/inugami/hardware-configuration.nix
+++ systems/inugami/hardware-configuration.nix
@@ -8,18 +8,13 @@
   boot = {
     initrd = {
       availableKernelModules = [ "xhci_pci"];
-      kernelModules = [ ];
     };
-    kernelModules = [ ];
-    extraModulePackages = [ ];
   };
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
     fsType = "ext4";
   };
-
-  swapDevices = [ ];
 
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = "aarch64-linux";
