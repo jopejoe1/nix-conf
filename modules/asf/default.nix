a3e275195505f66e0e95483d0085999199927532
jopejoe1 <johannes@joens.email>
2023-09-24 22:27:26 +0200

    Asf remove impurity

--- modules/asf/default.nix
+++ modules/asf/default.nix
@@ -5,7 +5,7 @@
     enable = true;
     web-ui.enable = true;
     settings = {
-      LicenseID = import /var/lib/asf/key.nix;
+      #LicenseID = import /var/lib/asf/key.nix;
       SteamProtocols = 7;
       AutoSteamSaleEvent = true;
     };
