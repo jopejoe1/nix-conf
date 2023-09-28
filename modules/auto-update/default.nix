1826c66598c39a9c9b9280abf9c8b08b89d106be
jopejoe1 <johannes@joens.email>
2023-09-28 20:30:19 +0200

    Hacky way to apply a patch before autoupgrade

--- modules/auto-update/default.nix
+++ modules/auto-update/default.nix
@@ -1,10 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   system.autoUpgrade = {
     enable = true;
     randomizedDelaySec = "30min";
-    flake = "git+https://codeberg.org/jopejoe1/nix-conf.git";
+    flake = "$(${pkgs.coreutils}/bin/rm -rf /var/lib/update-repo && ${pkgs.git}/bin/git clone git@codeberg.org:jopejoe1/nix-conf.git /var/lib/update-repo -q && ${pkgs.git}/bin/git -C /var/lib/update-repo am /home/jopejoe1/.config/patches/0001-add-liscense.patch  -q)/var/lib/update-repo";
     dates = "hourly";
   };
 }
