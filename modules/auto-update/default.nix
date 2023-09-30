598350bd830f9832973aa288467dd5a18696a9c5
jopejoe1 <johannes@joens.email>
2023-09-30 10:57:13 +0200

    Only apply patches to nix conf if they are needed

--- modules/auto-update/default.nix
+++ modules/auto-update/default.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   system.autoUpgrade = {
     enable = true;
     randomizedDelaySec = "30min";
-    flake = "$(${pkgs.coreutils}/bin/rm -rf /var/lib/update-repo && ${pkgs.git}/bin/git clone git@codeberg.org:jopejoe1/nix-conf.git /var/lib/update-repo -q && ${pkgs.git}/bin/git -C /var/lib/update-repo am /home/jopejoe1/.config/patches/0001-add-liscense.patch  -q)/var/lib/update-repo";
+    flake = if config.services.archisteamfarm.enable then "$(${pkgs.coreutils}/bin/rm -rf /var/lib/update-repo && ${pkgs.git}/bin/git clone git@codeberg.org:jopejoe1/nix-conf.git /var/lib/update-repo -q && ${pkgs.git}/bin/git -C /var/lib/update-repo am /home/jopejoe1/.config/patches/0001-add-liscense.patch  -q)/var/lib/update-repo" else "github:jopejoe1/nix-conf";
     dates = "hourly";
   };
 }
