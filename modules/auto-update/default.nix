e69dffe1ceb615de4fddc08ea825cc82c08c9c33
jopejoe1 <johannes@joens.email>
2023-10-12 17:25:02 +0200

    Dont pull full commit history

--- modules/auto-update/default.nix
+++ modules/auto-update/default.nix
@@ -4,7 +4,7 @@
   system.autoUpgrade = {
     enable = true;
     randomizedDelaySec = "30min";
-    flake = if config.services.archisteamfarm.enable then "$(${pkgs.coreutils}/bin/rm -rf /var/lib/update-repo && ${pkgs.git}/bin/git clone git@codeberg.org:jopejoe1/nix-conf.git /var/lib/update-repo -q && ${pkgs.git}/bin/git -C /var/lib/update-repo am /home/jopejoe1/.config/patches/0001-add-liscense.patch  -q)/var/lib/update-repo" else "github:jopejoe1/nix-conf";
+    flake = if config.services.archisteamfarm.enable then "$(${pkgs.coreutils}/bin/rm -rf /var/lib/update-repo && ${pkgs.git}/bin/git clone git@codeberg.org:jopejoe1/nix-conf.git /var/lib/update-repo -q --depth=1 && ${pkgs.git}/bin/git -C /var/lib/update-repo am /home/jopejoe1/.config/patches/0001-add-liscense.patch  -q)/var/lib/update-repo" else "github:jopejoe1/nix-conf";
     dates = "hourly";
   };
 }
