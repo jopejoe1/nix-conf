a44058466d9e93ca13df1d828848424a64c7a0c4
jopejoe1 <johannes@joens.email>
2023-10-12 17:59:28 +0200

    add nixd

--- modules/kate/default.nix
+++ modules/kate/default.nix
@@ -4,6 +4,7 @@
   environment.systemPackages = with pkgs; [
     libsForQt5.kate
     nil
+    nixd
     nodePackages_latest.bash-language-server
     nodePackages_latest.vscode-html-languageserver-bin
     nodePackages_latest.vscode-json-languageserver
