c9188ba1cd27caa1dbfa93a9264bd3242c723d30
jopejoe1 <johannes@joens.email>
2023-10-13 16:29:04 +0200

    should be self.inputs and not inputs

--- modules/theming/default.nix
+++ modules/theming/default.nix
@@ -1,9 +1,9 @@
-{pkgs, inputs, ...}:
+{pkgs, self, ...}:
 
 {
   stylix = {
     image = "${pkgs.libsForQt5.breeze-qt5}/share/wallpapers/Next/contents/images_dark/5120x2880.png";
     polarity = "dark";
-    base16Scheme = "${inputs.catppuccin-base16}/base16/frappe.yaml";
+    base16Scheme = "${self.inputs.catppuccin-base16}/base16/frappe.yaml";
   };
 }
