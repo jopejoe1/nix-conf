6be0729f36fea3ed7b7ce64eefc70fbaa995485b
jopejoe1 <johannes@joens.email>
2023-10-27 19:20:45 +0200

    add system feutre

--- systems/kami/default.nix
+++ systems/kami/default.nix
@@ -120,4 +120,8 @@
 
   # https://github.com/NixOS/nixpkgs/blob/c32c39d6f3b1fe6514598fa40ad2cf9ce22c3fb7/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix#L66
   boot.loader.systemd-boot.editor = false;
+
+  # Re-Compile everything for my specific cpu
+  nix.settings.system-features = ["gccarch-alderlake" "benchmark" "big-parallel" "kvm" "nixos-test"];
+
 }
