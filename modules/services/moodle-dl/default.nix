92e405472345885a42902d2f84bcd9f21462621c
jopejoe1 <johannes@joens.email>
2023-11-03 16:40:56 +0100

    moodle-dl: try with piping

--- modules/services/moodle-dl/default.nix
+++ modules/services/moodle-dl/default.nix
@@ -12,7 +12,7 @@
 
   systemd.services."moodle-dl" = {
     script = ''
-      #${lib.getExe pkgs.moodle-dl} --path /var/moodle-dl --add-all-visible-courses
+      ${pkgs.coreutils}/bin/echo "yes" | ${lib.getExe pkgs.moodle-dl} --path /var/moodle-dl --add-all-visible-courses
       ${lib.getExe pkgs.moodle-dl} --path /var/moodle-dl
       ${lib.getExe pkgs.git} -C /var/moodle-dl add .
       ${lib.getExe pkgs.git} -C /var/moodle-dl commit -m "moodle-dl updated on `$(${pkgs.coreutils}/bin/date)`"
