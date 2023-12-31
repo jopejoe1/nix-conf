08d4ff12f670da8bda9c3bf93d1fbb63f66c4d47
jopejoe1 <johannes@joens.email>
2023-12-31 22:10:20 +0100

    typo fix

--- modules/users/root/default.nix
+++ modules/users/root/default.nix
@@ -5,8 +5,8 @@ in {
   options.jopejoe1.root = { enable = lib.mkEnableOption "Enable root user"; };
 
   config = lib.mkIf cfg.enable {
-    users.users.root = { i
-      nitialPassword = "password";
+    users.users.root = {
+      initialPassword = "password";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB8oyMpS2hK3gQXyHIIVS6oilgMpemLmfhKKJ6RBMwUh johannes@joens.email"
       ];
