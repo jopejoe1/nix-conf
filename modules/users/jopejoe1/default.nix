88ae1b258d0840975720848f149f4203ef86bb1e
jopejoe1 <johannes@joens.email>
2023-12-31 21:28:43 +0100

    add ssh

--- modules/users/jopejoe1/default.nix
+++ modules/users/jopejoe1/default.nix
@@ -13,6 +13,9 @@ in {
       initialPassword = "password";
       extraGroups = [ "wheel" "networkmanager" "pipewire" "audio" ];
       uid = 1000;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB8oyMpS2hK3gQXyHIIVS6oilgMpemLmfhKKJ6RBMwUh johannes@joens.email"
+      ];
       packages = with pkgs;
         [ libsForQt5.kate libsForQt5.ark texlive.combined.scheme-full ]
         ++ lib.optionals (config.system == "x86_64-linux") [
--- modules/users/root/default.nix
+++ modules/users/root/default.nix
@@ -5,7 +5,12 @@ in {
   options.jopejoe1.root = { enable = lib.mkEnableOption "Enable root user"; };
 
   config = lib.mkIf cfg.enable {
-    users.users.root = { initialPassword = "password"; };
+    users.users.root = { i
+      nitialPassword = "password";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB8oyMpS2hK3gQXyHIIVS6oilgMpemLmfhKKJ6RBMwUh johannes@joens.email"
+      ];
+    };
   };
   imports = [ ./home.nix ];
 }
