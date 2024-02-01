4f0c7e1127c8c5f0062efcadbb1e70ddfcd62d9b
jopejoe1 <johannes@joens.email>
2024-02-01 16:35:17 +0100

    zerotire config stuff

--- modules/zerotierone/default.nix
+++ modules/zerotierone/default.nix
@@ -8,7 +8,7 @@ in {
 
   config = lib.mkIf cfg.enable {
     services.zerotierone.enable = true;
-    services.zerotierone.joinNetworks = [ "7c31a21e86f9a75c" "9e1948db638e9f93" ];
+    services.zerotierone.joinNetworks = [ "9e1948db638e9f93" ] ++ lib.optional (config.networking.hostName == "kuraokami") "7c31a21e86f9a75c";
     services.zerotierone.port = 9993;
     environment.systemPackages = with pkgs; [
       moonlight-qt
