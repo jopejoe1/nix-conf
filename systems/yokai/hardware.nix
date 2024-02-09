5ed7ba5109135a391bc717bcfa37cfe55f60e8f2
jopejoe1 <jopejoe1@noreply.codeberg.org>
2024-02-09 18:33:39 +0000

    Enable wireless network

--- systems/yokai/hardware.nix
+++ systems/yokai/hardware.nix
@@ -24,6 +24,8 @@
     size = (4 * 1024) + (2 * 1024);
   }];
 
+  networking.wireless.enable = true;
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
