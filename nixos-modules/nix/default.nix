576e92bf97f34b08cf633bb47323404cb853f1ed
jopejoe1 <johannes@joens.email>
2024-08-29 21:01:42 +0200

    add 2nd builder

--- nixos-modules/nix/default.nix
+++ nixos-modules/nix/default.nix
@@ -64,12 +64,22 @@ in
           {
             systems = getArchs "hetzner";
             supportedFeatures = self.nixosConfigurations.hetzner.config.nix.settings.system-features;
-            hostName = "missing.ninja";
+            hostName = "hetzner";
             publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUZYQzNPVGRPd0w4V1NDaHhFRENiNS9WbUZYcDZmWUZEM21MMFhFdzFFTDAgcm9vdEBoZXR6bmVyCg==";
             protocol = "ssh-ng";
             sshUser = "builder";
             sshKey = "/root/.ssh/builder";
             maxJobs = 12;
+          }
+          {
+            systems = getArchs "zap";
+            supportedFeatures = self.nixosConfigurations.zap.config.nix.settings.system-features;
+            hostName = "zap";
+            publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSU5Xb056T0QrM0UzT20xbGpkRUNxWUhaQkZteHA3bTNsS1MxSHBkQnZkZjYgcm9vdEB6YXAK";
+            protocol = "ssh-ng";
+            sshUser = "builder";
+            sshKey = "/root/.ssh/builder";
+            maxJobs = 4;
           }
         ];
       distributedBuilds = true;
