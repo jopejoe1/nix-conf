eb8f7dafa152b676e2ba060990d2968fb1e2d1f1
jopejoe1 <johannes@joens.email>
2024-10-08 10:15:20 +0200

    esp8266

--- packages/default.nix
+++ packages/default.nix
@@ -4,13 +4,18 @@
   system,
 }:
 
-{
+rec {
   libadwaita-follow-theme = pkgs.libadwaita.overrideAttrs (old: {
     patches = (old.patches or [ ]) ++ [ ./adwaita-theming-support.patch ];
     doCheck = false;
   });
 
   nixos-anywhere = inputs.nixos-anywhere.packages.${system}.nixos-anywhere;
+
+  esp8266 = pkgs.callPackage ./esp8266.nix { };
+  esp8266-sdk = pkgs.callPackage ./esp8266-sdk.nix {
+    inherit esp8266;
+  };
 
   kde-hdr-fix = pkgs.callPackage ./kde-hdr.nix { };
   kde-wallpaper = pkgs.callPackage ./kde-wallpaper.nix { };
