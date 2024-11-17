798031b8e977eaae106cb7a261140df6393d19cd
jopejoe1 <johannes@joens.email>
2024-11-17 23:16:56 +0100

    clean up inputs

--- flake.nix
+++ flake.nix
@@ -32,7 +32,6 @@
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-stable.follows = "nixpkgs";
     };
     impermanence.url = "github:nix-community/impermanence";
     snm = {
