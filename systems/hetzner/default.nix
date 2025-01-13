6963d7846165fb36631a36665df06af654ab4dee
jopejoe1 <jopejoe1@missing.ninja>
2025-01-13 16:45:09 +0100

    enable mastodon

--- systems/hetzner/default.nix
+++ systems/hetzner/default.nix
@@ -95,17 +95,20 @@ in
     chrootlocalUser = true;
   };
 
-  services.mastodon = {
-    enable = false;
-    streamingProcesses = (lib.elemAt config.facter.report.hardware.cpu 0).cores - 1;
-    localDomain = "nyan.social";
-    smtp.fromAddress = "mastodon@nyan.social";
-    configureNginx = true;
-  };
-
   users.users.backupftp = {
     isNormalUser = true;
     initialPassword = "backupPassword";
+  };
+
+  services.nginx = {
+    virtualHosts."${config.containers.nyan.config.services.mastodon.localDomain}" = {
+      forceSSL = true;
+      enableACME = true;
+      locations."/" = {
+        proxyPass = "http://192.168.100.5";
+        proxyWebsockets = true;
+      };
+    };
   };
 
   containers = {
@@ -115,6 +118,30 @@ in
       localAddress = "192.168.100.5/24";
       config = {
         system.stateVersion = "25.05";
+        services.mastodon = {
+          enable = true;
+          streamingProcesses = (lib.elemAt config.facter.report.hardware.cpu 0).cores - 1;
+          localDomain = "nyan.social";
+          smtp.fromAddress = "mastodon@nyan.social";
+          configureNginx = true;
+        };
+        services.nginx = {
+          virtualHosts."${config.containers.nyan.config.services.mastodon.localDomain}" = {
+            forceSSL = false;
+            enableACME = false;
+          };
+        };
+        networking = {
+          firewall = {
+            enable = true;
+            allowedTCPPorts = [
+              80
+            ];
+            allowedUDPPorts = [
+              80
+            ];
+          };
+        };
       };
     };
   };
