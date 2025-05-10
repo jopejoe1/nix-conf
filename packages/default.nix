5eb69904247ae7c5998aaf6399a670b429805fa7
jopejoe1 <jopejoe1@missing.ninja>
2025-05-10 19:49:53 +0200

    delet unused packages

--- packages/default.nix
+++ packages/default.nix
@@ -11,12 +11,4 @@ rec {
   });
 
   nixos-anywhere = inputs.nixos-anywhere.packages.${system}.nixos-anywhere;
-
-  esp8266 = pkgs.callPackage ./esp8266.nix { };
-  esp8266-sdk = pkgs.callPackage ./esp8266-sdk.nix {
-    inherit esp8266;
-  };
-
-  kde-hdr-fix = pkgs.callPackage ./kde-hdr.nix { };
-  kde-wallpaper = pkgs.callPackage ./kde-wallpaper.nix { };
 }
